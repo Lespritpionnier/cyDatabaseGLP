94a7cef0f7d1449ac37aceb6ff169c51f2d00a56
Leonardo <lespritpionnier@foxmail.com>
2020-02-25 10:25:56 +0100

    Constructors of tables

--- src/mainStructures/dataFramework/Table_database.java
+++ src/mainStructures/dataFramework/Table_database.java
@@ -2,6 +2,7 @@ package mainStructures.dataFramework;
 
 import mainStructures.dataFramework.dataDetails.KeyPrimary;
 import mainStructures.textExecutable.ExecutionTree;
+import mainStructures.toolsModule.pairVisitor.TreeVisitor;
 
 import java.util.ArrayList;
 
@@ -10,12 +11,21 @@ public class Table_database extends TableArchetype implements ExecutionTree {
     private ArrayList<Row_table> rows = new ArrayList<>();
     private ArrayList<String> columns = new ArrayList<>();
 
+   public Table_database(String name) { super(name); }
+   public Table_database(String name, ArrayList<Row_table> rows) {
+      super(name);
+      this.rows = rows;
+   }
 
-    public void addRow(){
+   public void addRow(){
         //TODO
     }
     public void addColumn(String name){
         //TODO
     }
 
+   @Override
+   public <T> T accept(TreeVisitor<T> visitor) {
+      return null;
+   }
 }
--- src/mainStructures/dataFramework/Table_warehouse.java
+++ src/mainStructures/dataFramework/Table_warehouse.java
@@ -6,7 +6,19 @@ import mainStructures.toolsModule.pairVisitor.TreeVisitor;
 
 public class Table_warehouse extends TableArchetype implements ExecutionTree {
     //Primary Key is in the first column
-    private Item_row[][] table = new Item_row[][];
+    private Item_row[][] table;
+
+    public Table_warehouse(String name) { super(name); }
+    public Table_warehouse(String name, Item_row[][] table) {
+        super(name);
+        this.table = table;
+    }
+    public Table_warehouse(String name, int dimRows, int dimColumns) {
+        super(name);
+        setDimRows(dimRows);
+        setDimColumns(dimColumns);
+        table = new Item_row[dimRows][dimColumns];
+    }
 
     @Override
     public <T> T accept(TreeVisitor<T> visitor) {
